976fb196b03178c4eaabb6ff3c03c1149287fce2
abdo <119325223+Abdobrh@users.noreply.github.com>
2026-05-01 11:39:57 +0100

    run flutter pub get

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,15 +6,23 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:audio_service_web/audio_service_web.dart';
+import 'package:audio_session/audio_session_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioServiceWeb.registerWith(registrar);
+  AudioSessionWeb.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
